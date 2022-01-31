8875b042d50a4981d741e765e82f3f999212366e
sanjoyksaha <sanjoyksaha0@gmail.com>
2022-02-01 00:37:42 +0600

    Relationship done.

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,14 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
-def __str__(self):
-    return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
